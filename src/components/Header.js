a14fc69fa0cca64648377f27686b247c3b24666b
hamzaakrammogul <hmughal502@gmail.com>
2023-06-19 18:13:17 +0500

    Implemented multiple slices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,30 @@
 import classes from './Header.module.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store';
+
 
 const Header = () => {
+const dispatch= useDispatch();
+
+const onClickHandler = ()=>{
+  dispatch(authActions.logout());
+}
+
+  const auth = useSelector(state => state.auth.isAuthenticated);
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
           <li>
-            <a href='/'>My Products</a>
+           {auth && <a href='/'>My Products</a>}
+          </li>
+          <li> 
+           {auth && <a href='/'>My Sales</a>}
           </li>
           <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
+            {auth && <button onClick={onClickHandler}>Logout</button>}
           </li>
         </ul>
       </nav>
--- src/store/index.js
+++ src/store/index.js
@@ -4,28 +4,51 @@ import { createSlice, configureStore } from '@reduxjs/toolkit';
 
 const counterSlice = createSlice({
 
-    name:'counter',
-    initialState: {counter:0, showCounter:true},
-    reducers:{
-        increment(state){
-            state.counter++;
+    name: 'counter',
+    initialState: { value: 0, showCounter: true },
+    reducers: {
+        increment(state) {
+            state.value++;
         },
-        decrement(state){
-            state.counter--;
+        decrement(state) {
+            state.value--;
         },
-        increase(state, action){
-            state.counter= state.counter+action.payload;
+        increase(state, action) {
+            state.value = state.value + action.payload;
         },
-        toggleCounter(state){
-            state.showCounter= !state.showCounter;
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
         }
     }
 });
 
-const store = configureStore({
-    reducer: counterSlice.reducer
- });
+const authSlice = createSlice({
+    name: 'Auth',
+    initialState: { isAuthenticated: false },
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        }
+    }
+});
 
- export const counterActions = counterSlice.actions;
- 
+// For single Reducer
+// const store = configureStore({
+//     reducer: counterSlice.reducer
+//  });
+
+//For Multiple Reducers
+const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer
+    }
+});
+
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
